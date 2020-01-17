160342eaad20b87d51c2495a223fd3ec461a3b5f
Anton Ziuzikov <zuzikov.anton@gmail.com>
2020-01-18 00:31:27 +0300

    feat: change colors of meta-info title and its content

--- YouTube-client/src/js/rendering/renderVideos.js
+++ YouTube-client/src/js/rendering/renderVideos.js
@@ -20,12 +20,18 @@ export default function renderVideos(videoArray) {
       title.setAttribute('href', `https://www.youtube.com/watch?v=${videoArray[i].id.videoId}`);
       const metaInfo = addNewElement('ul', 'main__video-preview__video__title__meta-info', video);
       const author = addNewElement('li', 'main__video-preview__video__meta-info_author', metaInfo);
-      author.innerHTML = `Channel: ${videoArray[i].snippet.channelTitle}`;
+      author.innerHTML = `Channel: `;
+      let span = addNewElement('span', 'meta-info_fetched-data', author);
+      span.innerHTML = `${videoArray[i].snippet.channelTitle}`;
       const date = addNewElement('li', 'main__video-preview__video__meta-info_publish-date', metaInfo);
       const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-      date.innerHTML = `Published: ${new Date(videoArray[i].snippet.publishedAt).toLocaleDateString('en-US', dateOptions)}`;
+      date.innerHTML = `Published: `;
+      span = addNewElement('span', 'meta-info_fetched-data', date);
+      span.innerHTML = `${new Date(videoArray[i].snippet.publishedAt).toLocaleDateString('en-US', dateOptions)}`;
       const views = addNewElement('li', 'main__video-preview__video__meta-info_views', metaInfo);
-      views.innerHTML = `Views: ${response.items[i].statistics.viewCount}`;
+      views.innerHTML = `Views: `;
+      span = addNewElement('span', 'meta-info_fetched-data', views);
+      span.innerHTML = `${response.items[i].statistics.viewCount}`;
       const description = addNewElement('p', 'main__video-preview__video__description', video);
       description.innerHTML = videoArray[i].snippet.description;
     }
